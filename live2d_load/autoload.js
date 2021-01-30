18379f3c7555740f4227d634e5c372ea85051749
indulge9451 <50791656+indulge9451@users.noreply.github.com>
2021-01-30 15:06:19 +0800

    Update autoload.js

--- live2d_load/autoload.js
+++ live2d_load/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/indulge9451/2d@master/live2d_load/";
-const tips_path = "https://cdn.jsdelivr.net/gh/indulge9451/2d@master/live2d_load/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/indulge9451/2d/live2d_load/";
+const tips_path = "https://cdn.jsdelivr.net/gh/indulge9451/2d/live2d_load/";
 
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
@@ -36,7 +36,7 @@ function loadExternalResource(url, type) {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
 			apiPath: "https://www.naosu.top/wp-content/themes/Sakurairo-master/live2d/2d_api",
-			cdnPath: "https://cdn.jsdelivr.net/gh/indulge9451/2d@master/api/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/indulge9451/2d/api/"
 
 		});
 	});
